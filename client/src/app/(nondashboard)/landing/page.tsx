9e8fbb4e97af58e2567eb3be372897cf212c1d96
longvuong19 <longvuong1920@gmail.com>
2025-10-19 23:07:27 +0900

    add discover and features sections

---
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import HeroSection from "./HeroSection";
+import FeaturesSection from "./FeaturesSection";
+import DiscoverSection from "./DiscoverSection";
+import CallToActionSection from "./CallToActionSection";
+
+const Landing = () => {
+  return (
+    <div>
+      <HeroSection />
+      <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
+    </div>
+  );
+};
+
+export default Landing;
